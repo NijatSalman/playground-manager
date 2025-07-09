6da86abad5dd82fc89724167756b57d1e38a1dfd
Nijat SAlmanov <nsalmanov@halleonard.com>
2025-07-09 09:54:45 +0300

    PM-001 project setup

---
+++ src/main/java/com/company/playgroundmanager/PlaygroundManagerApplication.java
@@ -0,0 +1,13 @@
+package com.company.playgroundmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaygroundManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaygroundManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/playgroundmanager/PlaygroundManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.playgroundmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaygroundManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
